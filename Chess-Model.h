41a048b62c2368da5e18aedc3c8d7dc059de6419
SahilRamdoss <udhay1royalist@gmail.com>
2025-10-07 14:52:23 +1100

    Created the structs and enums for iteration 1

--- Chess-Model.h
+++ Chess-Model.h
@@ -3,6 +3,7 @@
 
 
 using std::vector, std::string;
+const int BOARD_SIZE = 8;
 
 //...................................................................
 //.MMMMMM...MMMMMM...OOOOOOO.....DDDDDDDDD....EEEEEEEEEEE.LLLL.......
@@ -21,46 +22,72 @@ using std::vector, std::string;
 //...................................................................
 
 /**
- * @brief struct to represent the color of the current player
+ * @brief enum used to represent the type of the chess piece
  */
-
- enum PLAYER_COLOR
- {
-    WHITE,
-    BLACK
- };
-
- /**
-  * @brief Enum to represent the values of the different chess pieces
-  * 
-  */
- enum CHESS_PIECES
- {
+enum piece_type
+{
+    NONE = 0,
     PAWN = 1,
     KNIGHT = 3,
     BISHOP = 3,
     ROOK = 5,
-    QUEEN = 9
- };
-
-/**
- * @brief Struct entity to represent the state of the board
- */
-struct board{
-    string board_state;
-    vector<string> white_pieces_lost;
-    vector<string> black_pieces_lost;
-    vector<string> white_pieces_remaining;
-    vector<string> black_pieces_remaining;
+    QUEEN = 9,
+    KING = -1
 };
 
 /**
- * @brief Struct entity to represent the game being played
- * 
+ * @brief enum used to represent the color of chess pieces but also used to distinguish
+ * whose turn is it to play
+ */
+enum piece_color
+{
+    BLACK = 0,
+    WHITE = 1
+};
+
+/**
+ * @brief The struct is used to represent each chess piece
+ */
+struct chess_piece
+{
+    piece_type type; // The piece type
+    piece_color color; // The piece color
+};
+
+
+/**
+ * @brief This struct stores the state of the board
+ */
+struct board{
+    chess_piece board[BOARD_SIZE][BOARD_SIZE]; // The state of the board
+    vector <chess_piece> white_pieces_remaining; // White pieces reamining on the board
+    vector <chess_piece> black_pieces_remaining; // Black pieces remaining on the board
+    vector <chess_piece> white_pieces_lost; // White pieces removed from the board
+    vector <chess_piece> black_pieces_lost; // Black pieces removed from the board
+};
+
+/**
+ * @brief This struct is used to store the details for a particular game
  */
 struct game{
-    board game_board; // Used to store the state of the board state for the game
-    bool white_to_move; // To keep track of the current player's turn
-    int outcome; // To store the outcome of the game
+    board game_board; // Used to store the state of the board
+    piece_color active_player; // Used to store the colour of the current player
+    int outcome; // Used to store who won the game. (-1 for black, 0 for draw, 1 for white)
+};
+
+/**
+ * @brief struct used to represent a square on the chess board
+ */
+struct square{
+    int rank; // Represents a row on the chess board (1-8)
+    int file; // Represents a column on the chess board(1-8)
+};
+
+/**
+ * @brief struct used to represent a move made on the chess board
+ */
+struct move{
+    square from;
+    square to;
 };
 
